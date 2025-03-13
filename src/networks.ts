1b1de35e606060d0d5a3bd8667154da3f29ec5a9
bhflm <germanbonin@gmail.com>
2025-03-13 14:15:29 +0700

    chore: add eslint, prettier, and jest configurations, trying to keep consistency across the entire codebase

--- src/networks.ts
+++ src/networks.ts
@@ -1,5 +1,14 @@
-export const ETHEREUM_URL = "https://rail-squid.squids.live/squid-railgun-ethereum-v2/graphql";
-export const ARBITRUM_URL = "https://rail-squid.squids.live/squid-railgun-arbitrum-v2/graphql";
-export const ETHEREUM_SEPOLIA_URL = "https://rail-squid.squids.live/squid-railgun-eth-sepolia-v2/graphql";
-export const POLYGON_URL = "https://rail-squid.squids.live/squid-railgun-polygon-v2/graphql";
-export const BSC_URL = "https://rail-squid.squids.live/squid-railgun-bsc-v2/graphql";
+export const ETHEREUM_URL = 'https://rail-squid.squids.live/squid-railgun-ethereum-v2/graphql';
+export const ARBITRUM_URL = 'https://rail-squid.squids.live/squid-railgun-arbitrum-v2/graphql';
+export const ETHEREUM_SEPOLIA_URL =
+  'https://rail-squid.squids.live/squid-railgun-eth-sepolia-v2/graphql';
+export const POLYGON_URL = 'https://rail-squid.squids.live/squid-railgun-polygon-v2/graphql';
+export const BSC_URL = 'https://rail-squid.squids.live/squid-railgun-bsc-v2/graphql';
+
+export const VALID_SUBSQUID_URLS = [
+  ETHEREUM_URL,
+  ARBITRUM_URL,
+  ETHEREUM_SEPOLIA_URL,
+  POLYGON_URL,
+  BSC_URL,
+];
--- src/test.ts
+++ src/test.ts
@@ -1,22 +1,22 @@
 import { SubsquidClient, gql } from './client.js';
 
 const document = gql`
- {
-  shieldCommitments(limit: 10) {
-    blockNumber
+  {
+    shieldCommitments(limit: 10) {
+      blockNumber
+    }
   }
-}
 `;
 
 async function main() {
   try {
-    console.log("Initializing SubsquidClient...");
+    console.log('Initializing SubsquidClient...');
     const client = new SubsquidClient();
-    console.log("Sending request...");
+    console.log('Sending request...');
     const data = await client.request(document);
-    console.log("Response:", data);
+    console.log('Response:', data);
   } catch (error) {
-    console.error("Error occurred:", error);
+    console.error('Error occurred:', error);
   }
 }
 
