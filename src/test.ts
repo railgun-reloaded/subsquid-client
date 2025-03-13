27af3d37949dbcd86f49cacb79b7d8427b6d3ada
bhflm <germanbonin@gmail.com>
2025-03-13 12:47:26 +0700

    wip: tweak to tsconfig.info, replace graffle package, run test file ok

--- src/test.ts
+++ src/test.ts
@@ -1,21 +1,23 @@
-// import {SubsquidClient} from "./client.js"
-// @ts-ignore
-import { gql,SubsquidClient } from './client.js';
+import { SubsquidClient, gql } from './client.js';
 
-
-const document =  gql`
+const document = gql`
  {
   shieldCommitments(limit: 10) {
     blockNumber
   }
 }
-`
-async function main () {
+`;
 
-    console.log("here");
+async function main() {
+  try {
+    console.log("Initializing SubsquidClient...");
     const client = new SubsquidClient();
+    console.log("Sending request...");
     const data = await client.request(document);
-    console.log(data);  
-};
+    console.log("Response:", data);
+  } catch (error) {
+    console.error("Error occurred:", error);
+  }
+}
 
-main();
+main().catch(console.error);
