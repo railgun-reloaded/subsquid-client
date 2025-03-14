2e29140aa5124588e2cf1f596be9c24d256a4135
bhflm <germanbonin@gmail.com>
2025-03-14 14:11:00 +0700

    fix: typo

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// Re-export from client.ts
+export { SubsquidClient, isNetworkValid } from './client.js';
+// Re-export from networks.ts
+export { ETHEREUM_URL, ETHEREUM_SEPOLIA_URL, VALID_SUBSQUID_URLS } from './networks;;
+// Re-export gql for external use
+export { gql } from 'graphql-tag';
+// Note: These functions are for testing purposes only and not exported from example.ts
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJpbmRleC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQSwyQkFBMkI7QUFDM0IsT0FBTyxFQUFFLGNBQWMsRUFBRSxjQUFjLEVBQUUsTUFBTSxhQUFhLENBQUM7QUFFN0QsNkJBQTZCO0FBQzdCLE9BQU8sRUFBRSxZQUFZLEVBQUUsb0JBQW9CLEVBQUUsbUJBQW1CLEVBQUUsTUFBTSxZQUFZLENBQUE7QUFFcEYsaUNBQWlDO0FBQ2pDLE9BQU8sRUFBRSxHQUFHLEVBQUUsTUFBTSxhQUFhLENBQUM7QUFFbEMsdUZBQXVGIn0=
