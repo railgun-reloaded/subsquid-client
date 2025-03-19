5acc2015c8694e94c8619eb91830423e4d6ad715
bhflm <germanbonin@gmail.com>
2025-03-19 12:57:51 +0700

    chore: fix indentation within auto generated code, make function more concise

--- custom-plugin.ts
+++ custom-plugin.ts
@@ -1,14 +1,40 @@
 module.exports = {
   plugin(schema, _documents, _config) {
 
-  console.log('///////////////BEGIN CUSTOM PLUGIN///////////////');
-  // Get all query types from the schema
+  // HELPERS 
+  function capitalize(str) {                          
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }                                                       
+  // FINISH HELPERS 
 
-    // HELPERS 
-    function capitalize(str) {                          
-      return str.charAt(0).toUpperCase() + str.slice(1);
-    }                                                       
-    // FINISH HELPERS 
+  function printPreloadTypes() {
+    return `type ExtractFields<T, F extends (keyof T)[] | undefined> = F extends (keyof T)[]
+  ? Pick<T, F[number]>
+  : T;
+
+type AddFields<Args, TypeFields> = Args & { fields: (keyof TypeFields)[] }
+
+type GenerateIO<
+  Key extends keyof Query, 
+  QueryArgs,
+  Field = Query[Key],
+  Entity = Field extends Array<infer IT1>
+    ? IT1
+    : Field extends Maybe<infer IT2>
+      ? NonNullable<IT2>
+      : Field,            
+  Wrapper = Field extends Array<infer _>
+    ? 'array'
+    : Field extends Maybe<infer _>
+      ? 'maybe'
+      : 'simple'
+> = {
+  entity: Entity;
+  input: AddFields<QueryArgs, Entity>;
+  output: Field;
+  wrapper: Wrapper;
+}`
+  };
 
   function printQueryIO() {
     const queryType = schema.getQueryType();
@@ -17,13 +43,14 @@ module.exports = {
     }
     const queryFields = queryType.getFields();
     const queryFieldsNames = Object.keys(queryFields);
-    const queryIOTemplate = `type QueryIO = {
-        ${queryFieldsNames.map((fieldName) => `${fieldName}: GenerateIO<'${fieldName}', Query${capitalize(fieldName)}Args>\n`)}
-      }
-    `;
-    return queryIOTemplate;
+    
+    return `type QueryIO = {
+  ${queryFieldsNames.map((fieldName) => `${fieldName}: GenerateIO<'${fieldName}', Query${capitalize(fieldName)}Args>`).join('\n  ')}
+}`;
   };
 
-  return `${printQueryIO()}`;
+  return `${printPreloadTypes()}
+
+${printQueryIO()}`;
   }
 };
