69eb119c429a6dea272b37b9d87bbd301476ca1c
bhflm <germanbonin@gmail.com>
2025-03-31 16:47:14 +0200

    chore: rename QueryIO > EntityQueryMap

--- src/query-builder.ts
+++ src/query-builder.ts
@@ -1,4 +1,4 @@
-import type { FieldsArgs, FilterValue, QueryIO, QueryInput } from './types'
+import type { FieldsArgs, FilterValue, EntityQueryMap, QueryInput } from './types'
 
 /**
  * Builder class for constructing GraphQL queries
@@ -63,7 +63,7 @@ export class QueryBuilder {
    * @param value - The filter value with appropriate type based on FilterValue
    * @returns Formatted GraphQL argument string
    */
-  processFilter<K extends keyof QueryIO, F extends keyof QueryIO[K]['input']>(
+  processFilter<K extends keyof EntityQueryMap, F extends keyof EntityQueryMap[K]['input']>(
     _entityName: K,
     filterName: F,
     value: FilterValue<K, F>
@@ -101,11 +101,11 @@ export class QueryBuilder {
    * @param params.filters - The filters for the entity
    * @returns Formatted GraphQL query string
    */
-  private parseEntityQuery<K extends keyof QueryIO>(
-    { entityName, filters }: { entityName: K, filters: QueryIO[K]['input'] }
+  private parseEntityQuery<K extends keyof EntityQueryMap>(
+    { entityName, filters }: { entityName: K, filters: EntityQueryMap[K]['input'] }
   ): string {
-    // We know entity name is a key of QueryIO, force a cast type over it
-    const typedEntityName = entityName as keyof QueryIO
+    // We know entity name is a key of EntityQueryMap, force a cast type over it
+    const typedEntityName = entityName as keyof EntityQueryMap
     const fields = filters.fields as (FieldsArgs<typeof typedEntityName>)[]
 
     // Check that query has actually some fields requested data, if not is not a valid gql query
@@ -116,8 +116,8 @@ export class QueryBuilder {
     const filterArgs = Object.entries(filters)
       .filter(([name, _value]) => name !== 'fields') // Fields is handled separately from the rest
       .reduce((acc: string[], [name, value]) => {
-        const argName = name as keyof QueryIO[typeof typedEntityName]['input'] // type the argument name, some queries do not allow doing where or other filters, so we need to type it
-        const argValues = value as FilterValue<typeof typedEntityName, keyof QueryIO[typeof typedEntityName]['input']>
+        const argName = name as keyof EntityQueryMap[typeof typedEntityName]['input'] // type the argument name, some queries do not allow doing where or other filters, so we need to type it
+        const argValues = value as FilterValue<typeof typedEntityName, keyof EntityQueryMap[typeof typedEntityName]['input']>
 
         const processed = this.processFilter(typedEntityName, argName, argValues)
         if (processed) acc.push(processed)
@@ -146,8 +146,8 @@ export class QueryBuilder {
       query {
         ${entities
           .map(([entity, filtersForQuery]) => {
-            const entityName = entity as keyof QueryIO
-            const filters = filtersForQuery as QueryIO[typeof entityName]['input']
+            const entityName = entity as keyof EntityQueryMap
+            const filters = filtersForQuery as EntityQueryMap[typeof entityName]['input']
             const parsedQuery = this.parseEntityQuery({
               entityName,
               filters
--- src/types.ts
+++ src/types.ts
@@ -1,32 +1,32 @@
-import type { ExtractFields, QueryIO } from './generated/types'
+import type { ExtractFields, EntityQueryMap } from './generated/types'
 
 type QueryInput = {
-  [K in keyof QueryIO]?: QueryIO[K]['input'];
+  [K in keyof EntityQueryMap]?: EntityQueryMap[K]['input'];
 }
 
 // These types are for internal use and shouldn't be exported in index.ts
 type QueryOutput<T extends QueryInput> = {
-  [K in keyof T & keyof QueryIO]: T[K] extends { fields: (keyof QueryIO[K]['entity'])[] }
-    ? QueryIO[K]['wrapper'] extends 'array'
-      ? ExtractFields<QueryIO[K]['entity'], T[K]['fields']>[]
-      : ExtractFields<QueryIO[K]['entity'], T[K]['fields']>
-    : QueryIO[K]['output'];
+  [K in keyof T & keyof EntityQueryMap]: T[K] extends { fields: (keyof EntityQueryMap[K]['entity'])[] }
+    ? EntityQueryMap[K]['wrapper'] extends 'array'
+      ? ExtractFields<EntityQueryMap[K]['entity'], T[K]['fields']>[]
+      : ExtractFields<EntityQueryMap[K]['entity'], T[K]['fields']>
+    : EntityQueryMap[K]['output'];
 }
 
-type FilterValue<K extends keyof QueryIO, F extends keyof QueryIO[K]['input']> =
-  F extends 'fields' ? (keyof QueryIO[K]['entity'])[] :
-    F extends 'where' ? QueryIO[K]['input'] extends { where?: infer W } ? W : Record<string, any> :
-      F extends 'orderBy' ? QueryIO[K]['input'] extends { orderBy?: infer O } ? O : string[] :
+type FilterValue<K extends keyof EntityQueryMap, F extends keyof EntityQueryMap[K]['input']> =
+  F extends 'fields' ? (keyof EntityQueryMap[K]['entity'])[] :
+    F extends 'where' ? EntityQueryMap[K]['input'] extends { where?: infer W } ? W : Record<string, any> :
+      F extends 'orderBy' ? EntityQueryMap[K]['input'] extends { orderBy?: infer O } ? O : string[] :
         F extends 'limit' | 'offset' | 'first' ? number :
           F extends 'after' ? string :
             unknown
 
-type FieldsArgs<K extends keyof QueryIO> = keyof QueryIO[K]['entity']
+type FieldsArgs<K extends keyof EntityQueryMap> = keyof EntityQueryMap[K]['entity']
 
 export {
   type QueryInput,
   type QueryOutput,
   type FilterValue,
   type FieldsArgs,
-  type QueryIO
+  type EntityQueryMap
 }
