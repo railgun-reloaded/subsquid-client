11eb7d92b4cda09c3fd1b081c65e65b35c6e1668
bhflm <germanbonin@gmail.com>
2025-03-31 16:40:23 +0200

    chore: add jsdocs to generate-types

--- src/generate-types.ts
+++ src/generate-types.ts
@@ -1,59 +1,60 @@
+// @ts-nocheck
 module.exports = {
   /**
    * Generates TypeScript types based on the provided GraphQL schema.
-   * @param schema - The GraphQL schema object used to generate types.
-   * @param _documents - Unused parameter for document definitions.
+   * @param schema - The GraphQL schema object to generate types from.
+   * @param _documents - Unused parameter for additional documents.
    * @param _config - Unused parameter for configuration options.
    * @returns A string containing the generated TypeScript types.
    */
-  plugin (schema: any, _documents: any, _config: any) {
+  plugin: function (schema, _documents, _config) {
     if (!schema.getQueryType()) {
       throw new Error('No query type found in schema')
     }
 
     /**
-     * Capitalizes the first letter of a given string.
+     * Capitalizes the first letter of the given string.
      * @param str - The string to capitalize.
-     * @returns The input string with its first letter converted to uppercase.
+     * @returns The input string with the first letter converted to uppercase.
      */
-    function capitalize (str: string): string {
+    function capitalize (str) {
       return str.charAt(0).toUpperCase() + str.slice(1)
     }
 
     /**
-     * Generates TypeScript type definitions for preload-related utilities.
-     * @returns A string containing the TypeScript type definitions for ExtractFields, AddFields, and GenerateIO.
+     * Generates TypeScript types for preload operations.
+     * @returns A string containing the TypeScript type definitions for preload operations.
      */
     function printPreloadTypes () {
       const extractFieldsType =
-`export type ExtractFields<T, F extends (keyof T)[] | undefined> = F extends (keyof T)[]
-  ? Pick<T, F[number]>
-  : T;`
+        `export type ExtractFields<T, F extends (keyof T)[] | undefined> = F extends (keyof T)[]
+          ? Pick<T, F[number]>
+          : T;`
 
       const addFieldsType =
-'type AddFields<Args, TypeFields> = Args & { fields: (keyof TypeFields)[] }'
+        'type AddFields<Args, TypeFields> = Args & { fields: (keyof TypeFields)[] }'
 
       const generateIOType =
-`type GenerateIO<
-  Key extends keyof Query, 
-  QueryArgs,
-  Field = Query[Key],
-  Entity = Field extends Array<infer IT1>
-    ? IT1
-    : Field extends Maybe<infer IT2>
-      ? NonNullable<IT2>
-      : Field,            
-  Wrapper = Field extends Array<infer _>
-    ? 'array'
-    : Field extends Maybe<infer _>
-      ? 'maybe'
-      : 'simple'
-> = {
-  entity: Entity;
-  input: AddFields<QueryArgs, Entity>;
-  output: Field;
-  wrapper: Wrapper;
-}`
+        `type GenerateIO<
+          Key extends keyof Query, 
+          QueryArgs,
+          Field = Query[Key],
+          Entity = Field extends Array<infer IT1>
+            ? IT1
+            : Field extends Maybe<infer IT2>
+              ? NonNullable<IT2>
+              : Field,            
+          Wrapper = Field extends Array<infer _>
+            ? 'array'
+            : Field extends Maybe<infer _>
+              ? 'maybe'
+              : 'simple'
+        > = {
+          entity: Entity;
+          input: AddFields<QueryArgs, Entity>;
+          output: Field;
+          wrapper: Wrapper;
+        }`
       return [
         extractFieldsType,
         '',
@@ -61,15 +62,15 @@ module.exports = {
         '',
         generateIOType
       ].join('\n')
-    };
+    }
 
     /**
-     * Generates a key-value pair for a specific query field name.
-     * @param fieldName - The name of the query field.
-     * @returns A string representing the key-value pair for the query field.
+     * Generates a key-value pair for a given field name in the QueryIO type.
+     * @param fieldName - The name of the field to generate the key-value pair for.
+     * @returns A string representing the key-value pair for the QueryIO type.
      */
-    function generateTypeKV (fieldName: string) {
-    // Since there's no Query Args type for squidStatus, we don't need to handle the GenerateIO type for it
+    function generateTypeKV (fieldName) {
+      // Since there's no Query Args type for squidStatus, we don't need to handle the GenerateIO type for it
       if (fieldName === 'squidStatus') {
         return `${fieldName}: GenerateIO<'${fieldName}', {}>`
       }
@@ -77,17 +78,17 @@ module.exports = {
     }
 
     /**
-     * Generates TypeScript types for the QueryIO object based on the schema's query fields.
-     * @returns A string representing the TypeScript type definition for QueryIO.
+     * Generates the TypeScript type definition for QueryIO.
+     * @returns A string containing the TypeScript type definition for QueryIO.
      */
     function printQueryIO () {
       const queryType = schema.getQueryType()
       const queryFields = queryType.getFields()
       const queryFieldsNames = Object.keys(queryFields)
       return `export type QueryIO = {
-  ${queryFieldsNames.map(generateTypeKV).join('\n  ')}
-}`
-    };
+        ${queryFieldsNames.map(generateTypeKV).join('\n  ')}
+      }`
+    }
 
     // Note: Return like this with the weird indentation is to match the indentation of the original file
     return `${printPreloadTypes()}
