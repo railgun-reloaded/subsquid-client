f3a29ef96e22f5bf474e2b46843c03b185363211
bhflm <germanbonin@gmail.com>
2025-04-01 13:14:05 +0200

    fix: type generate-types export module

--- src/generate-types.ts
+++ src/generate-types.ts
@@ -1,5 +1,6 @@
-// @ts-nocheck
-module.exports = {
+import type { CodegenPlugin } from '@graphql-codegen/plugin-helpers'
+
+module.exports = <CodegenPlugin> {
   /**
    * Generates TypeScript types based on the provided GraphQL schema.
    * @param schema - The GraphQL schema object to generate types from.
@@ -69,7 +70,7 @@ module.exports = {
      * @param fieldName - The name of the field to generate the key-value pair for.
      * @returns A string representing the key-value pair for the QueryIO type.
      */
-    function generateTypeKV (fieldName) {
+    function generateTypeKV (fieldName: string) {
       // Since there's no Query Args type for squidStatus, we don't need to handle the GenerateIO type for it
       if (fieldName === 'squidStatus') {
         return `${fieldName}: GenerateIO<'${fieldName}', {}>`
@@ -83,7 +84,7 @@ module.exports = {
      */
     function printEntityQueryMap () {
       const queryType = schema.getQueryType()
-      const queryFields = queryType.getFields()
+      const queryFields = queryType!.getFields()
       const queryFieldsNames = Object.keys(queryFields)
       return `export type EntityQueryMap = {
         ${queryFieldsNames.map(generateTypeKV).join('\n  ')}
