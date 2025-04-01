0a2374e94b9deaa6f16f667e1bba3545233d8579
bhflm <germanbonin@gmail.com>
2025-04-01 13:08:42 +0200

    chore: remove generate operations.ts, not used anymore

--- codegen.ts
+++ codegen.ts
@@ -17,18 +17,7 @@ const config: CodegenConfig = {
         constEnums: true,
       },
     },
-    // Generate operation types and GraphQL request helpers
-    [path.join(__dirname, 'src/generated/operations.ts')]: {
-      schema: schemaFile,
-      plugins: ['typescript-operations', 'typescript-graphql-request'],
-      config: {
-        scalars: {
-          BigInt: 'string',
-          Bytes: 'string',
-        },
-      },
-    },
-  },
+  }
 };
 
 export default config;
